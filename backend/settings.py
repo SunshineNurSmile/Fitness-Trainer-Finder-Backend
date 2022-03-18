f02742ef466a0b6b5a0b7310a076dbd89a550a11
Li <llzzkk@bu.edu>
2022-03-18 03:01:12 -0400

    not delete static dir

--- backend/settings.py
+++ backend/settings.py
@@ -157,6 +157,22 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+# MEDIA_URL = '/images/'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR
+]
+
+# MEDIA_ROOT = "static/images"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SWAGGER_SETTINGS = {
