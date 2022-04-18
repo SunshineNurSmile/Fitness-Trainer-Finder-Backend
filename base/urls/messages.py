352c097414ab93e999a15db7871c23dedf462107
Li <llzzkk@bu.edu>
2022-04-18 00:44:33 -0400

    chat part

---
+++ base/urls/messages.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from base.views import users as views
+
+urlpatterns = [
+    path('create/', views.sendMessage, name="messages-send"),
+    path('list/', views.AllMessages, name='show_all_messages'),
+]
