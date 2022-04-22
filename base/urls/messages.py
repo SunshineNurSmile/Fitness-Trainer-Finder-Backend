79e02a126ebd162930580e2e43589126f9a34a23
Li <llzzkk@bu.edu>
2022-04-22 01:41:06 -0400

    add some descriptions add endDate to the order model

--- base/urls/messages.py
+++ base/urls/messages.py
@@ -2,6 +2,6 @@ from django.urls import path
 from base.views import users as views
 
 urlpatterns = [
-    path('create/', views.sendMessage, name="messages-send"),
-    path('list/<str:userid>/', views.AllMessages, name='show_all_messages'),
+    path('create/', views.sendMessage, name="messages-send"),  # To send message
+    path('list/<str:userid>/', views.AllMessages, name='show_all_messages'),  # To show the list of message
 ]
